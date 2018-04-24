3a7c6ca296e8f62ed3298f736515df577d1a8062
Ilya Solovyov <solovyov41@gmail.com>
2018-04-24 18:34:05 +0300

    Добавлена сборка в jar

--- ServiceRegistrationServer/src/main/java/ru/sberbank/vkr/microblog/serviceregistration/ServiceRegistrationServer.java
+++ ServiceRegistrationServer/src/main/java/ru/sberbank/vkr/microblog/serviceregistration/ServiceRegistrationServer.java
@@ -1,16 +1,18 @@
 package ru.sberbank.vkr.microblog.serviceregistration;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCloudDashboard
+@EnableDiscoveryClient
 public class ServiceRegistrationServer {
 
     public static void main(String[] args) {
-        // Tell Boot to look for registration-server.yml
-        System.setProperty("spring.config.name", "registration-server");
         SpringApplication.run(ServiceRegistrationServer.class, args);
     }
 }
