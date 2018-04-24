e449f5902ca6505471f2d958c6f7cc775a38f066
Kornext <kornext@gmail.com>
2018-04-24 17:16:07 +0300

    Add dashboard from eureka server

--- ServiceRegistrationServer/src/main/java/ru/sberbank/vkr/microblog/serviceregistration/ServiceRegistrationServer.java
+++ ServiceRegistrationServer/src/main/java/ru/sberbank/vkr/microblog/serviceregistration/ServiceRegistrationServer.java
@@ -1,11 +1,15 @@
 package ru.sberbank.vkr.microblog.serviceregistration;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCloudDashboard
+@EnableDiscoveryClient
 public class ServiceRegistrationServer {
 
     public static void main(String[] args) {
